efb924f93a058c5597be5fb00039e56cfec911b7
isabellabi-afk <isabellabi@alumni.mioti.es>
2026-05-24 13:43:51 +0200

    Fix import statement for TopicExplorer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Overview from "./pages/Overview";
-import TopicExplorer from "./pages/TopicExplorer";
+import {TopicExplorer}from "./pages/TopicExplorer";
 import MarketPosition from "./pages/MarketPosition";
 import Performance from "./pages/Performance";
 import MarketExplorer from "./pages/MarketExplorer";
